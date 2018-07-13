1223d0813681f8c4d8effdfa0d173bd8a2689fd5
apilguk@gmail.com <apilguk@gmail.com>
2018-07-13 17:14:31 +0300

    fix

--- lib/ops/threshold/kernel.glsl
+++ lib/ops/threshold/kernel.glsl
@@ -1,7 +1,6 @@
 /**
  * @license MIT
  * @author Arkadiy Pilguk(apilguk@gmail.com)
- * @author Mihail Zachepilo(mihailzachepilo@gmail.com)
  * Copyright 2018 Peculiar Ventures and Pentatonica.
  * All rights reserved.
  */
