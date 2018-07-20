d23f9d8f7520814ad690fc6c90add552023bdd84
worldthirteen <mihailzachepilo@gmail.com>
2018-07-20 18:24:36 +0300

    Add adaptive thershold operation and example

---
+++ app/src/examples/adaptive_threshold.js
@@ -0,0 +1,45 @@
+import * as gm from '../../../lib';
+
+export default {
+  op: (input, params) => {
+    let pipeline = input;
+
+    pipeline = gm.adaptiveThreshold(
+      gm.grayscale(pipeline),
+      params.THRESHOLD.uS,
+      params.THRESHOLD.uT,
+      params.THRESHOLD.c,
+    );
+
+    return pipeline;
+  },
+  params: {
+    THRESHOLD: {
+      name: 'THRESHOLD',
+      uS: {
+        name: 'Size',
+        type: 'uniform',
+        min: 0,
+        max: 100,
+        step: 1,
+        default: 5,
+      },
+      uT: {
+        name: 'Value',
+        type: 'uniform',
+        min: 0,
+        max: 100,
+        step: 1,
+        default: 15,
+      },
+      c: {
+        name: 'Channel',
+        type: 'constant',
+        min: 0,
+        max: 2,
+        step: 1,
+        default: 0,
+      },
+    },
+  },
+};
