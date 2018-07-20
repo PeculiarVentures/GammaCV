7a84ed0b09a8a409f79e3642269cea41c6258a32
apilguk@gmail.com <apilguk@gmail.com>
2018-07-20 16:56:54 +0300

    added math ops

---
+++ lib/ops/math/add.glsl
@@ -0,0 +1,14 @@
+/**
+ * @license MIT
+ * @author Arkadiy Pilguk(apilguk@gmail.com)
+ * @author Mihail Zachepilo(mihailzachepilo@gmail.com)
+ * Copyright 2018 Peculiar Ventures and Pentatonica.
+ * All rights reserved.
+ */
+
+vec4 operation(float y, float x) {
+  vec4 A = pickValue_tA(y, x);
+  vec4 B = pickValue_tB(y, x);
+
+	return vec4(A.rgb + B.rgb, 1.0);
+}
---
+++ lib/ops/math/mult_scalar.glsl
@@ -0,0 +1,13 @@
+/**
+ * @license MIT
+ * @author Arkadiy Pilguk(apilguk@gmail.com)
+ * @author Mihail Zachepilo(mihailzachepilo@gmail.com)
+ * Copyright 2018 Peculiar Ventures and Pentatonica.
+ * All rights reserved.
+ */
+
+vec4 operation(float y, float x) {
+  vec4 A = pickValue_tA(y, x);
+
+	return vec4(A.rgb * uScalar, 1.0);
+}
