0b2afecfeabe3e83b891efebf0c5bac2bbb45830
worldthirteen <mihailzachepilo@gmail.com>
2018-07-23 23:09:10 +0300

    Fix adaptive thershold operation bug and docs

--- lib/ops/adaptive_threshold/kernel.glsl
+++ lib/ops/adaptive_threshold/kernel.glsl
@@ -6,20 +6,23 @@
  * All rights reserved.
  */
 
+const int Channel = int(C);
+
 vec4 operation(float y, float x) {
 	vec4 pixel = pickValue_tSrc(y, x);
-	vec4 value = pickValue_tIntegralImage(y, x);
+	float huS = uS / 2.0;
 	
-	vec2 p1 = max(vec2(x, y) - uS / 2.0, vec2(1.0));
-	vec2 p2 = min(vec2(x, y) + uS / 2.0, OUT_VIEW);
+	vec2 p1 = max(floor(vec2(x, y) - huS), vec2(1.0));
+	vec2 p2 = min(floor(vec2(x, y) + huS), OUT_VIEW - 1.0);
 	vec2 pd = p2 - p1;
 	float s = pd.x * pd.y;
-	float sum = pickValue_tIntegralImage(p2.y, p2.x).r
-		- pickValue_tIntegralImage(p2.y, p1.x - 1.0).r
-		- pickValue_tIntegralImage(p1.y - 1.0, p2.x).r
-		+ pickValue_tIntegralImage(p1.y - 1.0, p1.x - 1.0).r;
+	p1 -= 1.0;
+	float sum = pickValue_tIntegralImage(p2.y, p2.x)[Channel]
+		- pickValue_tIntegralImage(p1.y, p2.x)[Channel]
+		- pickValue_tIntegralImage(p2.y, p1.x)[Channel]
+		+ pickValue_tIntegralImage(p1.y, p1.x)[Channel];
 
-	if (pixel[int(C)] * s <= sum * (100.0 - uT)/ 100.0) {
+	if (pixel[Channel] * s <= sum * (100.0 - uT)/ 100.0) {
 		return vec4(0.0, 0.0, 0.0, 1.0);	
 	} else {
 		return vec4(1.0, 1.0, 1.0, 1.0);
