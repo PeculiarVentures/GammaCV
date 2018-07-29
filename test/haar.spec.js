ee9a4df19ae4e711fd51826fe911cf0eb1135d8d
apilguk@gmail.com <apilguk@gmail.com>
2018-07-29 22:22:49 +0300

    ordering fix

--- test/haar.spec.js
+++ test/haar.spec.js
@@ -51,8 +51,8 @@ describe.only('HAAR Features', () => {
     sess.runOp(op, Math.random(), out);
 
     const f1 = gm.calcHAARFeature(out, feature1, 20, 15, 15, 20); // this value should be close to 0
-    const f2 = gm.calcHAARFeature(out, feature2, 20, 65, 15, 20); // this value should be close to 0
-    const f3 = gm.calcHAARFeature(out, feature3, 20, 15, 65, 20); // this value should be close to 0
+    const f2 = gm.calcHAARFeature(out, feature2, 20, 15, 65, 20); // this value should be close to 0
+    const f3 = gm.calcHAARFeature(out, feature3, 20, 65, 15, 20); // this value should be close to 0
     const f4 = gm.calcHAARFeature(out, feature4, 20, 65, 65, 20); // this value should be close to 0
 
     console.log(f1, f2, f3, f4);
@@ -67,8 +67,8 @@ describe.only('HAAR Features', () => {
     sess.runOp(op, Math.random(), out);
 
     const f1 = gm.calcHAARFeature(out, feature1, 40, 30, 30, 20); // this value should be close to 0
-    const f2 = gm.calcHAARFeature(out, feature2, 40, 130, 30, 20); // this value should be close to 0
-    const f3 = gm.calcHAARFeature(out, feature3, 40, 30, 130, 20); // this value should be close to 0
+    const f2 = gm.calcHAARFeature(out, feature2, 40, 30, 130, 20); // this value should be close to 0
+    const f3 = gm.calcHAARFeature(out, feature3, 40, 130, 30, 20); // this value should be close to 0
     const f4 = gm.calcHAARFeature(out, feature4, 40, 130, 130, 20); // this value should be close to 0
 
     console.log(f1, f2, f3, f4);
@@ -83,8 +83,8 @@ describe.only('HAAR Features', () => {
     sess.runOp(op, Math.random(), out);
 
     const f1 = gm.calcHAARFeature(out, feature1, 60, 45, 45, 20); // this value should be close to 0
-    const f2 = gm.calcHAARFeature(out, feature2, 60, 195, 45, 20); // this value should be close to 0
-    const f3 = gm.calcHAARFeature(out, feature3, 60, 45, 195, 20); // this value should be close to 0
+    const f2 = gm.calcHAARFeature(out, feature2, 60, 45, 195, 20); // this value should be close to 0
+    const f3 = gm.calcHAARFeature(out, feature3, 60, 195, 45, 20); // this value should be close to 0
     const f4 = gm.calcHAARFeature(out, feature4, 60, 195, 195, 20); // this value should be close to 0
 
     console.log(f1, f2, f3, f4);
