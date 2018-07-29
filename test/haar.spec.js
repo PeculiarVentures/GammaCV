3da0551e6f7caac901b6d1cb30bacf8401933882
worldthirteen <mihailzachepilo@gmail.com>
2018-07-30 00:38:23 +0300

    Enhance haar features

--- test/haar.spec.js
+++ test/haar.spec.js
@@ -2,7 +2,6 @@ import { assert } from 'chai';
 import testImage100 from './assets/haar_test_100.png';
 import testImage200 from './assets/haar_test_200.png';
 import testImage300 from './assets/haar_test_300.png';
-
 import * as gm from '../lib';
 
 describe.only('HAAR Features', () => {
@@ -50,15 +49,18 @@ describe.only('HAAR Features', () => {
     sess.init(op);
     sess.runOp(op, Math.random(), out);
 
-    const f1 = gm.calcHAARFeature(out, feature1, 20, 15, 15, 20); // this value should be close to 0
-    const f2 = gm.calcHAARFeature(out, feature2, 20, 15, 65, 20); // this value should be close to 0
-    const f3 = gm.calcHAARFeature(out, feature3, 20, 65, 15, 20); // this value should be close to 0
-    const f4 = gm.calcHAARFeature(out, feature4, 20, 65, 65, 20); // this value should be close to 0
+    const f1 = gm.calcHAARFeature(out, feature1, 20, 15, 15, 20);
+    const f2 = gm.calcHAARFeature(out, feature2, 20, 15, 65, 20);
+    const f3 = gm.calcHAARFeature(out, feature3, 20, 65, 15, 20);
+    const f4 = gm.calcHAARFeature(out, feature4, 20, 65, 65, 20);
 
-    console.log(f1, f2, f3, f4);
+    assert.closeTo(f1, 200, 1);
+    assert.closeTo(f2, -200, 1);
+    assert.closeTo(f3, 200, 1);
+    assert.closeTo(f4, 200, 1);
   });
 
-  it('test200', async () => {
+  it.only('test200', async () => {
     const input = await gm.imageTensorFromURL(testImage200);
     const op = gm.summedAreaTable(gm.grayscale(input));
     const out = gm.tensorFrom(op);
@@ -66,12 +68,15 @@ describe.only('HAAR Features', () => {
     sess.init(op);
     sess.runOp(op, Math.random(), out);
 
-    const f1 = gm.calcHAARFeature(out, feature1, 40, 30, 30, 20); // this value should be close to 0
-    const f2 = gm.calcHAARFeature(out, feature2, 40, 30, 130, 20); // this value should be close to 0
-    const f3 = gm.calcHAARFeature(out, feature3, 40, 130, 30, 20); // this value should be close to 0
-    const f4 = gm.calcHAARFeature(out, feature4, 40, 130, 130, 20); // this value should be close to 0
+    const f1 = gm.calcHAARFeature(out, feature1, 40, 30, 30, 20);
+    const f2 = gm.calcHAARFeature(out, feature2, 40, 30, 130, 20);
+    const f3 = gm.calcHAARFeature(out, feature3, 40, 130, 30, 20);
+    const f4 = gm.calcHAARFeature(out, feature4, 40, 130, 130, 20);
 
-    console.log(f1, f2, f3, f4);
+    assert.closeTo(f1, 800, 2);
+    assert.closeTo(f2, -800, 2);
+    assert.closeTo(f3, 800, 2);
+    assert.closeTo(f4, 800, 2);
   });
 
   it('test300', async () => {
@@ -82,11 +87,14 @@ describe.only('HAAR Features', () => {
     sess.init(op);
     sess.runOp(op, Math.random(), out);
 
-    const f1 = gm.calcHAARFeature(out, feature1, 60, 45, 45, 20); // this value should be close to 0
-    const f2 = gm.calcHAARFeature(out, feature2, 60, 45, 195, 20); // this value should be close to 0
-    const f3 = gm.calcHAARFeature(out, feature3, 60, 195, 45, 20); // this value should be close to 0
-    const f4 = gm.calcHAARFeature(out, feature4, 60, 195, 195, 20); // this value should be close to 0
+    const f1 = gm.calcHAARFeature(out, feature1, 60, 45, 45, 20);
+    const f2 = gm.calcHAARFeature(out, feature2, 60, 45, 195, 20);
+    const f3 = gm.calcHAARFeature(out, feature3, 60, 195, 45, 20);
+    const f4 = gm.calcHAARFeature(out, feature4, 60, 195, 195, 20);
 
-    console.log(f1, f2, f3, f4);
+    assert.closeTo(f1, 1800, 1);
+    assert.closeTo(f2, -1800, 1);
+    assert.closeTo(f3, 1800, 1);
+    assert.closeTo(f4, 1800, 1);
   });
 });
