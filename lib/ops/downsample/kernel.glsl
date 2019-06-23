9cbf1805345e8e13e9f0ed09df754960ba66dabd
WorldThirteen <mihailzachepilo@gmail.com>
2019-06-23 19:03:46 +0300

    Cancel extra dev changes

--- lib/ops/downsample/kernel.glsl
+++ lib/ops/downsample/kernel.glsl
@@ -7,33 +7,25 @@
  */
 
 vec4 operation(float y, float x) {
-  float r = 0.0;
-  float g = 0.0;
-  float b = 0.0;
+  vec4 value = vec4(0.0);
   
   for (float dx = 0.0; dx < K; dx += 1.0) {
     for (float dy = 0.0; dy < K; dy += 1.0) {
-      vec4 v = pickValue_tSrc((y * K) - dy, (x * K) - dx);
+      vec4 v = pickValue_tSrc((y * K) + dy, (x * K) + dx);
 
       if (S == 0.0) {
-        r = v.r;
-        g = v.g;
-        b = v.b;
+        value = v;
       } 
 
       if (S == 1.0) {
-        r += v.r;
-        g += v.g;
-        b += v.b;
+        value += v;
       } 
     }
   }
 
   if (S == 1.0) {
-    r /= K * K;
-    g /= K * K;
-    b /= K * K;
+    value /= K * K;
   } 
 
-	return vec4(r, g, b, 1.0);
+	return value;
 }
