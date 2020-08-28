5a508832cac1a614dcbac15e1e3c00a0cf1d6b53
apilguk <apilguk@gmail.com>
2020-08-28 15:22:07 +0300

    feat: ability to use HTMLCanvasElement or HTMLVideoElement as input of operation

--- test/program/media_input.spec.js
+++ test/program/media_input.spec.js
@@ -1,17 +1,8 @@
 import { assert } from 'chai';
 import * as gm from '../../lib';
 
-describe('Operation', () => {
+describe('MediaInput', () => {
   let sess;
-  const testOp = tSrc => new gm.RegisterOperation('Test')
-    .Input('tSrc', 'uint8')
-    .Output('uint8')
-    .GLSLKernel(`
-      vec4 operation(float y, float x) {
-        return vec4(0, 1, 0, 1);
-      }
-    `)
-    .Compile({ tSrc });
 
   before(async () => {
     if (sess) {
@@ -21,100 +12,42 @@ describe('Operation', () => {
     sess = new gm.Session();
   });
 
-  it('create operation with name', () => {
-    const opName = 'Test';
-    const op = new gm.Operation(opName);
+  it('run pipeline with canvas element', () => {
+    const canvas = document.createElement('canvas');
+    const canvasCtx = canvas.getContext('2d');
 
-    assert.equal(op.name, `Test:${op.id}`, 'Operation name should be concatenated with it id');
-  });
+    canvas.width = 1;
+    canvas.height = 1;
 
-  it('traverse', () => {
-    const input = new gm.Tensor('uint8', [1, 1, 4]);
-    const op = testOp(input);
-    let numTraversedNodes = 0;
+    canvasCtx.fillStyle = 'rgba(255, 0, 0, 1)';
+    canvasCtx.fillRect(0, 0, 1, 1);
 
-    op.traverse(() => {
-      numTraversedNodes += 1;
-    }, numTraversedNodes);
-
-    assert.equal(numTraversedNodes, 2, 'Should traverse itself and input');
-  });
-
-  it('getDependencies', () => {
-    const input = new gm.Tensor('uint8', [1, 1, 4]);
-    const op1 = testOp(input);
-    const op2 = testOp(op1);
-    const deps = op2.getDependencies();
-
-    assert.deepEqual(deps, [op1.name, op2.name], 'should have dependent of child operation');
-  });
-
-  it('assignInput', () => {
-    const a = new gm.Tensor('uint8', [1, 1, 4]);
-    const b = new gm.Tensor('uint8', [1, 1, 4]);
-
-    const op = testOp(a);
-    const input1Name = a.name;
-
-    op.assignInput('tSrc', b);
-
-    const input2Name = op.input.tSrc.name;
-
-    assert.notEqual(input2Name, input1Name, 'input should be reassigned');
-  });
-
-  it('clone', () => {
-    const input = new gm.Tensor('uint8', [1, 1, 4]);
-    const op = testOp(input);
-    const opCloned = op.clone();
-
-    assert.notEqual(op.name, opCloned.name, 'should have different name');
-    assert.notEqual(op.id, opCloned.id, 'should have different id');
-    assert.equal(op.dtype, opCloned.dtype, 'should clone dtype');
-    assert.equal(op.kernel, opCloned.kernel, 'should clone kernel');
-    assert.deepEqual(op.chunks, opCloned.chunks, 'should clone chunks');
-    assert.deepEqual(op.uniform, opCloned.uniform, 'should clone chunks');
-    assert.deepEqual(op.constant, opCloned.constant, 'should clone chunks');
-  });
-
-  it('init', () => {
-    const input = new gm.Tensor('uint8', [1, 1, 4]);
-    const op = testOp(input);
-
-    sess.init(op);
-
-    // TODO: More concretic needed
-    assert.isTrue(op.isInitialized, 'should be initialized by session');
-  });
-
-  it('able to run in sess', () => {
-    const input = new gm.Tensor('uint8', [1, 1, 4]);
-    const op = testOp(input);
+    const input = new gm.MediaInput(canvas, [1, 1, 4]);
+    const op = gm.cast(input, 'uint8');
     const out = gm.tensorFrom(op);
 
     sess.init(op);
-
     sess.runOp(op, 0, out);
 
-    // make sure kernel works
-    assert.equal(out.data[0], 0, 'should be 0');
-    assert.equal(out.data[1], 255, 'should be 255');
+    // make sure op works correctly
+    assert.equal(out.data[0], 255, 'should be 0');
+    assert.equal(out.data[1], 0, 'should be 0');
     assert.equal(out.data[2], 0, 'should be 0');
     assert.equal(out.data[3], 255, 'should be 255');
   });
 
-  it('able to run op wit media input', () => {
-    const input = new gm.MediaInput(document.createElement('video'), [1, 1, 4]);
-    const op = testOp(input);
+  it('run pipeline with video element', () => {
+    const video = document.createElement('video');
+    const input = new gm.MediaInput(video, [1, 1, 4]);
+    const op = gm.grayscale(input);
     const out = gm.tensorFrom(op);
 
     sess.init(op);
-
     sess.runOp(op, 0, out);
 
-    // make sure kernel works
+    // make sure op works correctly
     assert.equal(out.data[0], 0, 'should be 0');
-    assert.equal(out.data[1], 255, 'should be 255');
+    assert.equal(out.data[1], 0, 'should be 0');
     assert.equal(out.data[2], 0, 'should be 0');
     assert.equal(out.data[3], 255, 'should be 255');
   });
