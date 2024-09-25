0edcfc92817e1192059a5682360f8cdd97f77301
worldthirteen <mihailzachepilo@gmail.com>
2024-09-25 10:31:45 +0000

    Updates

---
+++ _next/static/chunks/219.e5a85e2eb60159fc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[219],{5219:function(e,s,t){t.r(s);var u=t(6243);s.default={op:(e,s)=>{let t=e;return t=u.se(t),t=u.Ig(t,s.COLORSEGMENTATION.clusters)},params:{COLORSEGMENTATION:{name:"COLOR SEGMENTATION",clusters:{name:"Num Clusters",type:"constant",min:2,max:255,step:1,default:3}}}}}}]);
