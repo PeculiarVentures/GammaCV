32c4eb8d502e8b6c6a2d159a0aa0139648e34509
worldthirteen <mihailzachepilo@gmail.com>
2024-12-18 21:08:01 +0000

    Updates

--- _next/static/chunks/494.8e0472ef8d004165.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[494],{3494:function(e,a,n){n.r(a);var t=n(6243);a.default={op:(e,a)=>t.Lj(e,a.THRESHOLD.uT,a.THRESHOLD.c),params:{THRESHOLD:{name:"THRESHOLD",uT:{name:"Value",type:"uniform",min:0,max:1,step:.05,default:.1},c:{name:"Channel",type:"constant",min:0,max:2,step:1,default:0}}}}}}]);
---
+++ _next/static/chunks/581.a2376b61dabba5bd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[581],{7581:function(e,a,n){n.r(a);var t=n(6243);a.default={op:(e,a)=>t.Lj(e,a.THRESHOLD.uT,a.THRESHOLD.c),params:{THRESHOLD:{name:"THRESHOLD",uT:{name:"Value",type:"uniform",min:0,max:1,step:.05,default:.1},c:{name:"Channel",type:"constant",min:0,max:2,step:1,default:0}}}}}}]);
